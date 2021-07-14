f8beb235dcb0c84f06c627efbc4344aa248c314d
hamed zabihi <hamed0zabihi@gmail.com>
2021-07-14 18:50:27 +0430

    :globe_with_meridians: for move to server

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,11 @@ import thunk from "redux-thunk";
 import { getAllCourses } from "../actions/courses";
 import { reducers } from "../reducers/";
 
-export const store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+//for debuging
+// const ReduxDevTools =
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+export const store = createStore(reducers, compose(applyMiddleware(thunk)));
 //dispatch
 store.dispatch(getAllCourses());
 //subscribe
